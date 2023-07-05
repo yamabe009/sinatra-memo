bdc29992b3b7a04a1d51c83bfdbd1af0d530aeb5
yamabe009 <yamabe.kinoko@gmail.com>
2023-07-05 22:08:00 +0900

    memo/1にアクセスすると、id=1のメモのタイトルと内容が表示されるようにした

--- memo_app.rb
+++ memo_app.rb
@@ -18,3 +18,10 @@ post '/memo/new' do
   @status_msg = '登録しました'
   erb :memo_saved
 end
+
+get '/memo/:id' do
+  file = File.open('memo/memo.json')
+  list = JSON.parse(file.read, symbolize_names: true)
+  @item = list.find { |i| i[:id] == params['id'].to_i }
+  erb :memo_details
+end
